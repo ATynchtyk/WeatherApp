18212b3f0521a3217617393574d80f75dc04af57
ATynchtyk <98882256+ATynchtyk@users.noreply.github.com>
2022-05-25 23:43:00 +0600

    WeatherApp Bashtaldy

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: const Padding(
+          padding:  EdgeInsets.only(left: 5.0),
+          child:  Icon(
+            Icons.navigation,
+            size: 50.0,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.location_city,
+                size: 50.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/weather.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                '8\u00B0⛅️',
+                style: TextStyle(
+                    fontSize: 100.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Жылуу кийин',
+                style: TextStyle(
+                    fontSize: 80,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+              Text(
+                'Бишкек',
+                style: TextStyle(fontSize: 50.0, color: Colors.white),
+              ),
+            ]),
+      ),
+    );
+  }
+}
